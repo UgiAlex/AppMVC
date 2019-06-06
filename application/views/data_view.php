09ac6c55cb92e5e599fd984ccc1ee04438bf602a
UgiAlex <51223900+UgiAlex@users.noreply.github.com>
2019-06-06 16:37:46 +0700

    Add files via upload

--- application/views/data_view.php
+++ application/views/data_view.php
@@ -5,6 +5,6 @@
 	<h3><?= $row['Title'] ?></h3>
 	<p><?= $row['TimePublic'] ?></p>
 	<p>Комментариев: <?= $row['Comments'] ?></p>
-	<a href="/data/post/<?= $row['id'] ?>">Читать</a>
-	<hr>
+	<a href="/data/post/<?= $row['id'] ?>">Читать</a>	<hr>
+
 <?php endforeach; ?>

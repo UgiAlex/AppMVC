b5b82f5c2d2b766946694a203e2fcf6732bbd84e
UgiAlex <51223900+UgiAlex@users.noreply.github.com>
2019-06-22 13:39:45 +0700

    Delete post_view.php

--- application/views/post_view.php
+++
@@ -1,9 +0,0 @@
-﻿<?php foreach ($data as $row):	?>
-
-<h3><?= $row['Title'] ?></h3>
-<p><?= $row['TimePublic'] ?></p>
-<p>Комментариев: <?= $row['Comments'] ?></p>
-<a href="/post/<?= $row['id'] ?>">Читать</a>
-<hr>
-
-<?php endforeach; ?>
